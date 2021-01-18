d3c87b83488503337356482ce18ac525ab0b4d08
Thibault <derbomez.thibault@gmail.com>
2021-01-18 23:32:30 +0100

    ajout condition commande + message

--- src/app/repas/repas.component.ts
+++ src/app/repas/repas.component.ts
@@ -15,17 +15,18 @@ import { UserService } from '../shared/services/user.service';
 export class RepasComponent implements OnInit {
   public meals: Meal[];
   public user: User;
+  public message: string = null
+  public messageType: boolean = null
 
   constructor(
     private mealService: MealService,
     private orderService: OrderService,
     private userService: UserService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.mealService.findAllAvailableForToday().subscribe((meals: Meal[]) => {
       this.meals = meals;
-      console.log(this.meals);
     });
 
     this.user = this.userService.getCurrentUser();
@@ -33,15 +34,40 @@ export class RepasComponent implements OnInit {
 
   public addCommande(meal) {
     if (window.confirm('Passer la commande ?')) {
-      let order = new Order(this.user.id, 1, [new Quantity(1, meal.id)]);
-      this.orderService.addOrder(order).subscribe(
-        (order: Order) => {
-          console.log(order);
-        },
-        (err) => {
-          console.log(err);
-        }
-      );
+      if (this.user.wallet >= meal.priceDF) {
+
+        let order = new Order(this.user.id, 1, [new Quantity(1, meal.id)]);
+        this.orderService.addOrder(order).subscribe(
+          (order: Order) => {
+            this.message = "Commande effectuer"
+            this.messageType = true
+
+            setTimeout(() => {
+              this.message = null
+              this.messageType = null
+            }, 3000);
+          },
+          (err) => {
+            console.log();
+            this.message = err.error.exceptionMessage
+            this.messageType = false
+
+            setTimeout(() => {
+              this.message = null
+              this.messageType = null
+            }, 3000);
+          }
+        );
+      }
+      else {
+        this.message = "Fond insufisant, merci d'aller voir la cantiniere"
+        this.messageType = false
+
+        setTimeout(() => {
+          this.message = null
+          this.messageType = null
+        }, 3000);
+      }
     }
   }
 
